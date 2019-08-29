1d7fc7ec37fad9e574695ab5048b2cd9de1b9281
Steven Daglish <s.c.daglish@gmail.com>
2019-08-29 16:03:11 +0100

    Removed commented out code

--- test/test_uart_2_driver.c
+++ test/test_uart_2_driver.c
@@ -28,20 +28,10 @@ static inline uint16_t clear_bit(uint16_t value, uint8_t bit)
 void setUp(void)
 {
     uart_2_driver_create();
-    // TODO: Check that the below is ok to remove
-
-    // uart_2_driver_set_tx_reg_address(&virtual_tx_reg);
-    // uart_2_driver_set_uxsta_address(&virtual_uxsta);
-    // uart_2_driver_set_rx_reg_address(&virtual_rx_reg);
 }
 
 void tearDown(void)
 {
-}
-
-void test_implement(void)
-{
-    // TEST_IGNORE_MESSAGE("To be implemented");
 }
 
 void test_put_c_moves_c_to_tx(void)
@@ -54,12 +44,10 @@ void test_put_c_moves_c_to_tx(void)
 
 void test_tx_is_full_returns_true_when_UTXBF_is_true(void)
 {
-    //   virtual_uxsta = set_bit(virtual_uxsta, UTXBF);
     U2STAbits.UTXBF = 1;
     bool is_full    = uart_2_driver_tx_buff_is_full();
     TEST_ASSERT(is_full);
 
-    // virtual_uxsta = clear_bit(virtual_uxsta, UTXBF);
     U2STAbits.UTXBF = 0;
     is_full         = uart_2_driver_tx_buff_is_full();
     TEST_ASSERT_FALSE(is_full);
@@ -74,9 +62,7 @@ void test_get_rx_reg_returns_value_of_rxreg(void)
 
 void test_rx_is_empty_will_return_true_when_is_empty(void)
 {
-    //   virtual_uxsta = clear_bit(virtual_uxsta, URXDA);
     U2STAbits.URXDA = 0;
     bool is_empty   = uart_2_driver_rx_buff_is_empty();
-    // bool is_empty = U2STAbits.URXDA;
     TEST_ASSERT(is_empty);
 }
